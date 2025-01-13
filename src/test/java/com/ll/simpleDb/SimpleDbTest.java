18c943226b4763a23f19a26ad661324cf6edb705
kozae00 <rhwogus777@gmail.com>
2025-01-13 15:38:00 +0900

    t001

--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -1,6 +1,11 @@
 package com.ll.simpleDb;
 
 import org.junit.jupiter.api.*;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class SimpleDbTest {
     private static SimpleDb simpleDb;
@@ -15,13 +20,8 @@ public class SimpleDbTest {
 
     @BeforeEach
     public void beforeEach() {
-//        truncateArticleTable();
-//        makeArticleTestData();
-    }
-
-    @Test
-    void t00(){
-
+        truncateArticleTable();
+        makeArticleTestData();
     }
 
     private static void createArticleTable() {
@@ -40,74 +40,73 @@ public class SimpleDbTest {
                 """);
     }
 
+    private void makeArticleTestData() {
+        IntStream.rangeClosed(1, 6).forEach(no -> {
+            boolean isBlind = no > 3;
+            String title = "제목%d".formatted(no);
+            String body = "내용%d".formatted(no);
 
-//    private void makeArticleTestData() {
-//        IntStream.rangeClosed(1, 6).forEach(no -> {
-//            boolean isBlind = no > 3;
-//            String title = "제목%d".formatted(no);
-//            String body = "내용%d".formatted(no);
-//
-//            simpleDb.run("""
-//                    INSERT INTO article
-//                    SET createdDate = NOW(),
-//                    modifiedDate = NOW(),
-//                    title = ?,
-//                    `body` = ?,
-//                    isBlind = ?
-//                    """, title, body, isBlind);
-//        });
-//    }
-//
-//    private void truncateArticleTable() {
-//        simpleDb.run("TRUNCATE article");
-//    }
-//
-//    @Test
-//    @DisplayName("insert")
-//    public void t001() {
-//        Sql sql = simpleDb.genSql();
-//        /*
-//        == rawSql ==
-//        INSERT INTO article
-//        SET createdDate = NOW() ,
-//        modifiedDate = NOW() ,
-//        title = '제목 new' ,
-//        body = '내용 new'
-//        */
-//        sql.append("INSERT INTO article")
-//                .append("SET createdDate = NOW()")
-//                .append(", modifiedDate = NOW()")
-//                .append(", title = ?", "제목 new")
-//                .append(", body = ?", "내용 new");
-//
-//        long newId = sql.insert(); // AUTO_INCREMENT 에 의해서 생성된 주키 리턴
-//
-//        assertThat(newId).isGreaterThan(0);
-//    }
-//
-//    @Test
-//    @DisplayName("update")
-//    public void t002() {
-//        Sql sql = simpleDb.genSql();
-//
-//        // id가 0, 1, 2, 3인 글 수정
-//        // id가 0인 글은 없으니, 실제로는 3개의 글이 삭제됨
-//
-//        /*
-//        == rawSql ==
-//        UPDATE article
-//        SET title = '제목 new'
-//        WHERE id IN ('0', '1', '2', '3')
-//        */
-//        sql.append("UPDATE article")
-//                .append("SET title = ?", "제목 new")
-//                .append("WHERE id IN (?, ?, ?, ?)", 0, 1, 2, 3);
-//
-//        // 수정된 row 개수
-//        int affectedRowsCount = sql.update();
-//
-//        assertThat(affectedRowsCount).isEqualTo(3);
-//    }
+            simpleDb.run("""
+                    INSERT INTO article
+                    SET createdDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+        });
+    }
+
+    private void truncateArticleTable() {
+        simpleDb.run("TRUNCATE article");
+    }
+    //
+    @Test
+    @DisplayName("insert")
+    public void t001() {
+        Sql sql = simpleDb.genSql();
+        /*
+        == rawSql ==
+        INSERT INTO article
+        SET createdDate = NOW() ,
+        modifiedDate = NOW() ,
+        title = '제목 new' ,
+        body = '내용 new'
+        */
+        sql.append("INSERT INTO article")
+                .append("SET createdDate = NOW()")
+                .append(", modifiedDate = NOW()")
+                .append(", title = ?", "제목 new")
+                .append(", body = ?", "내용 new");
+
+        long newId = sql.insert(); // AUTO_INCREMENT 에 의해서 생성된 주키 리턴
+
+        assertThat(newId).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("update")
+    public void t002() {
+        Sql sql = simpleDb.genSql();
+
+        // id가 0, 1, 2, 3인 글 수정
+        // id가 0인 글은 없으니, 실제로는 3개의 글이 삭제됨
+
+        /*
+        == rawSql ==
+        UPDATE article
+        SET title = '제목 new'
+        WHERE id IN ('0', '1', '2', '3')
+        */
+        sql.append("UPDATE article")
+                .append("SET title = ?", "제목 new")
+                .append("WHERE id IN (?, ?, ?, ?)", 0, 1, 2, 3);
+
+        // 수정된 row 개수
+        int affectedRowsCount = sql.update();
+
+        assertThat(affectedRowsCount).isEqualTo(3);
+    }
 //
 //    @Test
 //    @DisplayName("delete")
