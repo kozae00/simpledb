02fc8be342be7fb47f7288ce83f46ee2fe4899f2
kozae00 <rhwogus777@gmail.com>
2025-01-13 14:40:06 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
+    kotlin("jvm")
 }
 
-group = "org.example"
+group = "com.ll"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,10 +11,24 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    runtimeOnly("com.mysql:mysql-connector-j:9.1.0")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
 }
